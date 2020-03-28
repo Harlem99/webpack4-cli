495f968bf469e93b948f6237e0ff7bf89d4dcb87
HLJMT <harlem097@163.com>
2020-03-28 23:05:56 +0800

    html配置

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 8088,
+        compress: true,
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        open: true
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'HLHMT-PAGE',
+            filename: 'index.html',
+            template: 'index.html',
+            inject: false
+        })
+    ]
+};
